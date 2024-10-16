7ec12e4b2c8c52c7e83876d888586074f98de04e
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-10-16 12:02:33 +0530

    added database and prisma

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -1,14 +1,24 @@
 import {Request, Response} from "express"
 import { ApiError } from "../utils/errorHandler";
 import { asyncHandler } from "../utils/asyncHandler";
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
 // @desc  Get all contacts
 // @route  GET /api/contacts
 // @access public
 
 export const getAllContacts = asyncHandler( async (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "Get all contacts"
-    })
+    try {
+        const allContacts = await prisma.contact.findMany({})
+        return res.status(200).json({
+            message: "Get all contacts",
+            contacts: allContacts
+        })
+    } catch (error) {
+        throw new ApiError(500,  "Internal server error")
+    }  
 } )
 
 // @desc  create new contact
@@ -19,8 +29,17 @@ export const createContact = asyncHandler( async (req: Request, res: Response) =
     if(!name || !email || !phone) {
         throw new ApiError(400, "All fields are mandatory")
     }
-    
-    res.status(200).json({
+    const newContact = await prisma.contact.create({
+        data: {
+            name,
+            email,
+            phone
+        },
+    })
+    if(!newContact) {
+        throw new ApiError(500, "Unable to create the contact")
+    }
+    return res.status(201).json({
         message: "Contract Created Successfully"
     })
 } )
@@ -28,18 +47,52 @@ export const createContact = asyncHandler( async (req: Request, res: Response) =
 // @desc  Get contact
 // @route GET /api/contacts/:id
 // @access public
-export const getContact = asyncHandler( async (req: Request, res: Response) => {
-    res.status(200).json({
-        message: `Return contact with id ${req.params.id}`
-    })
-} )
+export const getContact = asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params; // Destructure id directly
+    if (!id) {
+        throw new ApiError(400, "Id is required");
+    }
+    
+    const contact = await prisma.contact.findUnique({
+        where: { id },
+    });
+    if (!contact) {
+        throw new ApiError(404, "Contact not found");
+    }
+    return res.status(200).json({
+        message: "Contact retrieved successfully",
+        contact,
+    });
+});
 
 // @desc  Update contact
 // @route PUT /api/contacts/:id
 // @access public
 export const updateContact = asyncHandler( async (req: Request, res: Response) => {
-    res.status(200).json({
-        message: `Update contact with id ${req.params.id}`
+    const id = req.params.id;
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone) {
+        throw new ApiError(400, "All fields are mandatory")
+    }
+    if(!id) {
+        throw new ApiError(400, "Id is required")
+    }
+    const updatedContact = await prisma.contact.update({
+        where: {
+            id
+        },
+        data: {
+            name,
+            email,
+            phone
+        }
+    })
+    if(!updatedContact) {
+        throw new ApiError(500, "Contact cannot be updated")
+    }
+    return res.status(200).json({
+        message: `Contact updated with id ${req.params.id}`,
+        contact: updatedContact
     })
 } )
 
@@ -47,7 +100,19 @@ export const updateContact = asyncHandler( async (req: Request, res: Response) =
 // @route DELETE /api/contacts/:id
 // @access public
 export const deleteContact = asyncHandler( async (req: Request, res: Response) => {
-    res.status(200).json({
+    const id = req.params.id;
+    if(!id) {
+        throw new ApiError(400, "Id is required")
+    }
+    const deletedContact = await prisma.contact.delete({
+        where: {
+            id
+        }
+    })
+    if(!deletedContact) {
+        throw new ApiError(500, "Contact not deleted")
+    }
+    return res.status(200).json({
         message: `Delete contact with id ${req.params.id}`
     })
 } )
